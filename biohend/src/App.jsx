7dabb4463783e3010763e4603072fab48ffeb777
EnesHHayrula <eneshayrula@gmail.com>
2023-11-11 23:58:36 +0200

    Converted Banner to jsx

--- biohend/src/App.jsx
+++ biohend/src/App.jsx
@@ -1,4 +1,5 @@
 import About from "./components/About";
+import Banner from "./components/Banner";
 import ChooseUs from "./components/ChooseUs";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
@@ -21,25 +22,7 @@ function App() {
         <Header />
 
         {/* banner */}
-        <section className="banner_main">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-12 ">
-                <div className="text-bg">
-                  <h1>FARMING COMPANY</h1>
-                  <p>
-                    It is a long established fact that a reader will be
-                    distracted by the readable content of a page when looking at
-                    its layout. The point of using Lorem Ipsum is that it has a
-                    more-or-less normal distribution of letters, as opposed to
-                    using 'Content here, content here', making it
-                  </p>
-                  <a href="#">Discover</a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
+        <Banner />
         {/* end banner */}
         {/* three_box */}
         <div className="three_box">
---
+++ biohend/src/components/Banner.jsx
@@ -0,0 +1,23 @@
+export default function Banner() {
+  return (
+    <section className="banner_main">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12 ">
+            <div className="text-bg">
+              <h1>FARMING COMPANY</h1>
+              <p>
+                It is a long established fact that a reader will be distracted
+                by the readable content of a page when looking at its layout.
+                The point of using Lorem Ipsum is that it has a more-or-less
+                normal distribution of letters, as opposed to using 'Content
+                here, content here', making it
+              </p>
+              <a href="#">Discover</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
