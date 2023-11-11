6f52e4172ccb8623e15f19d6eef63a938156fdf8
EnesHHayrula <eneshayrula@gmail.com>
2023-11-12 00:01:49 +0200

    Converted three pics section to jsx

---
+++ biohend/src/components/ThreePics.jsx
@@ -0,0 +1,31 @@
+export default function ThreePics() {
+  return (
+    <div className="three_box">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <div className="box_text">
+              <figure>
+                <img src="images/img1.jpg" alt="#" />
+              </figure>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="box_text">
+              <figure>
+                <img src="images/img2.jpg" alt="#" />
+              </figure>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="box_text">
+              <figure>
+                <img src="images/img3.jpg" alt="#" />
+              </figure>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
