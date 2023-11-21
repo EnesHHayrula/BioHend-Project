fda32b2c5830b7714594bbb5a263d6e2683cd7e3
EnesHHayrula <eneshayrula@gmail.com>
2023-11-21 13:48:23 +0200

    changed about us page

--- biohend/src/components/about/About.jsx
+++ biohend/src/components/about/About.jsx
@@ -10,7 +10,7 @@ const About = () => {
             <div className="row">
               <div className="col-md-12">
                 <div className="titlepage">
-                  <h2>People Says About Farm</h2>
+                  <h2>About us</h2>
                 </div>
               </div>
             </div>
@@ -21,27 +21,14 @@ const About = () => {
                   className="carousel slide about_Carousel "
                   data-ride="carousel"
                 >
-                  <ol className="carousel-indicators">
-                    <li
-                      data-target="#myCarousel"
-                      data-slide-to={0}
-                      className="active"
-                    />
-                    <li data-target="#myCarousel" data-slide-to={1} />
-                    <li data-target="#myCarousel" data-slide-to={2} />
-                  </ol>
                   <div className="carousel-inner">
                     <div className="carousel-item active">
                       <div className="container">
                         <div className="carousel-caption ">
                           <div className="row">
                             <div className="col-md-12">
-                              <div className="test_box">
-                                <i>
-                                  <img src="images/tes1.png" alt="#" />
-                                </i>
-                                <h4>jhone Du</h4>
-                                <p>
+                              <div className="text_box">
+                                <span>
                                   It is a long established fact that a reader
                                   will be distracted by the readable content of
                                   a page when looking at its layout. The point
@@ -57,73 +44,7 @@ const About = () => {
                                   page when looking at its layout. The point of
                                   using Lorem Ipsum is that it has a
                                   more-or-less normal distribution of letters,
-                                </p>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="carousel-item">
-                      <div className="container">
-                        <div className="carousel-caption">
-                          <div className="row">
-                            <div className="col-md-12">
-                              <div className="test_box">
-                                <i>
-                                  <img src="images/tes1.png" alt="#" />
-                                </i>
-                                <h4>jhone Du</h4>
-                                <p>
-                                  It is a long established fact that a reader
-                                  will be distracted by the readable content of
-                                  a page when looking at its layout. The point
-                                  of using Lorem Ipsum is that it has a
-                                  more-or-less normal distribution of letters,It
-                                  is a long established fact that a reader will
-                                  be distracted by the readable content of a
-                                  page when looking at its layout. The point of
-                                  using Lorem Ipsum is that it has a
-                                  more-or-less normal distribution of letters,It
-                                  is a long established fact that a reader will
-                                  be distracted by the readable content of a
-                                  page when looking at its layout. The point of
-                                  using Lorem Ipsum is that it has a
-                                  more-or-less normal distribution of letters,
-                                </p>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="carousel-item">
-                      <div className="container">
-                        <div className="carousel-caption">
-                          <div className="row">
-                            <div className="col-md-12">
-                              <div className="test_box">
-                                <i>
-                                  <img src="images/tes1.png" alt="#" />
-                                </i>
-                                <h4>jhone Du</h4>
-                                <p>
-                                  It is a long established fact that a reader
-                                  will be distracted by the readable content of
-                                  a page when looking at its layout. The point
-                                  of using Lorem Ipsum is that it has a
-                                  more-or-less normal distribution of letters,It
-                                  is a long established fact that a reader will
-                                  be distracted by the readable content of a
-                                  page when looking at its layout. The point of
-                                  using Lorem Ipsum is that it has a
-                                  more-or-less normal distribution of letters,It
-                                  is a long established fact that a reader will
-                                  be distracted by the readable content of a
-                                  page when looking at its layout. The point of
-                                  using Lorem Ipsum is that it has a
-                                  more-or-less normal distribution of letters,
-                                </p>
+                                </span>
                               </div>
                             </div>
                           </div>
@@ -131,30 +52,6 @@ const About = () => {
                       </div>
                     </div>
                   </div>
-                  <a
-                    className="carousel-control-prev"
-                    href="#myCarousel"
-                    role="button"
-                    data-slide="prev"
-                  >
-                    <span
-                      className="carousel-control-prev-icon"
-                      aria-hidden="true"
-                    />
-                    <span className="sr-only">Previous</span>
-                  </a>
-                  <a
-                    className="carousel-control-next"
-                    href="#myCarousel"
-                    role="button"
-                    data-slide="next"
-                  >
-                    <span
-                      className="carousel-control-next-icon"
-                      aria-hidden="true"
-                    />
-                    <span className="sr-only">Next</span>
-                  </a>
                 </div>
               </div>
             </div>
