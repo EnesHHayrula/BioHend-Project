32cf01a46b108495a23b8739097ea889356a6775
EnesHHayrula <eneshayrula@gmail.com>
2023-11-21 22:35:37 +0200

    Added cart page and css

--- biohend/src/components/cart/Cart.jsx
+++ biohend/src/components/cart/Cart.jsx
@@ -9,7 +9,7 @@ const Cart = () => {
         <div className="container">
           <div className="row">
             <div className="col-lg-12">
-              <div className="table-main table-responsive">
+              <div className="table-main table-bordered table-responsive">
                 <table className="table">
                   <thead>
                     <tr>
@@ -25,17 +25,46 @@ const Cart = () => {
                     <tr>
                       <td className="thumbnail-img">
                         <a href="#">
-                          <img
-                            className="img-fluid"
-                            src="/public/images/product2.jpg"
-                          />
+                        <span className="cart-image" style={{backgroundImage: 'url(/public/images/product1.jpg)'}}> </span>
+                          
+                        </a>
+                      </td>
+                      <td className="name-pr">
+                        <a href="#">Lavender Water</a>
+                      </td>
+                      <td className="price-pr">
+                        <p>$ 5.00</p>
+                      </td>
+                      <td className="quantity-box">
+                        <input
+                          type="number"
+                          size={4}
+                          defaultValue={1}
+                          min={0}
+                          step={1}
+                          className="c-input-text qty text"
+                        />
+                      </td>
+                      <td className="total-pr">
+                        <p>$ 5.00</p>
+                      </td>
+                      <td className="remove-pr">
+                        <a href="#">
+                          <CiSquareRemove />
+                        </a>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td className="thumbnail-img">
+                        <a href="#">
+                        <span className="cart-image" style={{backgroundImage: 'url(/public/images/product2.jpg)'}}> </span>
                         </a>
                       </td>
                       <td className="name-pr">
                         <a href="#">Lavender Oil</a>
                       </td>
                       <td className="price-pr">
-                        <p>$ 10.0</p>
+                        <p>$ 12.00</p>
                       </td>
                       <td className="quantity-box">
                         <input
@@ -48,7 +77,7 @@ const Cart = () => {
                         />
                       </td>
                       <td className="total-pr">
-                        <p>$ 10.0</p>
+                        <p>$ 12.00</p>
                       </td>
                       <td className="remove-pr">
                         <a href="#">
@@ -59,18 +88,14 @@ const Cart = () => {
                     <tr>
                       <td className="thumbnail-img">
                         <a href="#">
-                          <img
-                            className="img-fluid"
-                            src="images/product1.jpg"
-                            alt=""
-                          />
+                        <span className="cart-image" style={{backgroundImage: 'url(/public/images/product3.jpg)'}}> </span>
                         </a>
                       </td>
                       <td className="name-pr">
-                        <a href="#">Lorem ipsum dolor sit amet</a>
+                        <a href="#">Worm Tea</a>
                       </td>
                       <td className="price-pr">
-                        <p>$ 60.0</p>
+                        <p>$ 15.00</p>
                       </td>
                       <td className="quantity-box">
                         <input
@@ -83,42 +108,7 @@ const Cart = () => {
                         />
                       </td>
                       <td className="total-pr">
-                        <p>$ 80.0</p>
-                      </td>
-                      <td className="remove-pr">
-                        <a href="#">
-                          <CiSquareRemove />
-                        </a>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td className="thumbnail-img">
-                        <a href="#">
-                          <img
-                            className="img-fluid"
-                            src="images/img-pro-03.jpg"
-                            alt=""
-                          />
-                        </a>
-                      </td>
-                      <td className="name-pr">
-                        <a href="#">Lorem ipsum dolor sit amet</a>
-                      </td>
-                      <td className="price-pr">
-                        <p>$ 30.0</p>
-                      </td>
-                      <td className="quantity-box">
-                        <input
-                          type="number"
-                          size={4}
-                          defaultValue={1}
-                          min={0}
-                          step={1}
-                          className="c-input-text qty text"
-                        />
-                      </td>
-                      <td className="total-pr">
-                        <p>$ 80.0</p>
+                        <p>$ 15.00</p>
                       </td>
                       <td className="remove-pr">
                         <a href="#">
@@ -131,14 +121,7 @@ const Cart = () => {
               </div>
             </div>
           </div>
-          <div className="row my-5">
-            <div className="col-lg-6 col-sm-6"></div>
-            <div className="col-lg-6 col-sm-6">
-              <div className="update-box">
-                <input defaultValue="Update Cart" type="submit" />
-              </div>
-            </div>
-          </div>
+          
           <div className="row my-5">
             <div className="col-lg-8 col-sm-12" />
             <div className="col-lg-4 col-sm-12">
@@ -146,17 +129,17 @@ const Cart = () => {
                 <h3>Order summary</h3>
                 <div className="d-flex">
                   <h4>Sub Total</h4>
-                  <div className="ml-auto font-weight-bold"> $ 130 </div>
+                  <div className="ml-auto font-weight-bold"> $ 32.00 </div>
                 </div>
                 <hr className="my-1" />
                 <div className="d-flex">
                   <h4>Shipping Cost</h4>
-                  <div className="ml-auto font-weight-bold"> Free </div>
+                  <div className="ml-auto font-weight-bold"> $ 7.00 </div>
                 </div>
                 <hr />
                 <div className="d-flex gr-total">
                   <h5>Grand Total</h5>
-                  <div className="ml-auto h5"> $ 388 </div>
+                  <div className="ml-auto h5"> $ 39.00 </div>
                 </div>
                 <hr />{" "}
               </div>
