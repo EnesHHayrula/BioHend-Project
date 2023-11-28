d46503f027e89090795ec977f615ba06555d186e
EnesHHayrula <eneshayrula@gmail.com>
2023-11-28 23:25:23 +0200

    no message

--- biohend/src/components/home/Home.jsx
+++ biohend/src/components/home/Home.jsx
@@ -3,30 +3,6 @@ import { Link } from "react-router-dom";
 export default function Home() {
   return (
     <div>
-      <meta charSet="utf-8" />
-      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta name="viewport" content="initial-scale=1, maximum-scale=1" />
-      <title>jon</title>
-      <meta name="keywords" content />
-      <meta name="description" content />
-      <meta name="author" content />
-      <link rel="stylesheet" href="css/bootstrap.min.css" />
-      <link rel="stylesheet" href="css/style.css" />
-      <link rel="stylesheet" href="css/responsive.css" />
-      <link rel="icon" href="images/fevicon.png" type="image/gif" />
-      <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css" />
-      <link
-        rel="stylesheet"
-        href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css"
-      />
-      <link
-        rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css"
-        media="screen"
-      />
-      {/*[endif]--*/}
-
       <section className="banner_main">
         <div className="container">
           <div className="row">
