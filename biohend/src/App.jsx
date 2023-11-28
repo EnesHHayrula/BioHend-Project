4ab3b7209abbfe835bc7660eefae33674a31b33b
EnesHHayrula <eneshayrula@gmail.com>
2023-11-28 23:25:05 +0200

    upgrade app

--- biohend/src/App.jsx
+++ biohend/src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Loader from "./components/Loader";
 import Home from "./components/home/Home";
 import Layout from "./components/layout/Layout";
-import Account from "./components/accountModal/Account";
 import About from "./components/about/About";
 import Blog from "./components/blog/Blog";
 import Cart from "./components/cart/Cart";
@@ -17,7 +16,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="my-account" element={<Account />} />
           <Route path="about" element={<About />} />
           <Route path="blog" element={<Blog />} />
           <Route path="contact" element={<Contact />} />
