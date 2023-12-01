a675a42121b529f0d3a838133b79d677cad3dd94
EnesHHayrula <eneshayrula@gmail.com>
2023-12-01 22:03:37 +0200

    path utiils and path added

---
+++ biohend/src/paths.js
@@ -0,0 +1,9 @@
+const Path = {
+    Home: '/',
+    Logout: '/logout',
+    GameEdit: '/games/:gameId/edit',
+    GameDelete: '/games/:gameId/delete',
+    GameDetails: '/games/:gameId',
+};
+
+export default Path;
