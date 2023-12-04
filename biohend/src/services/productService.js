cb6f5b9f0137f9ce87602eb4cea3636e3c650b4e
EnesHHayrula <eneshayrula@gmail.com>
2023-12-04 11:56:53 +0200

    product service added

---
+++ biohend/src/services/productService.js
@@ -0,0 +1,25 @@
+import * as request from "../lib/request";
+
+const baseUrl = "http://localhost:3030/data/shop";
+
+export const getAll = async () => {
+  const result = await request.get(baseUrl);
+
+  return result;
+};
+
+export const getOne = async (productId) => {
+  const result = await request.get(`${baseUrl}/${productId}`);
+
+  return result;
+};
+
+export const getLatest = async () => {
+  const query = new URLSearchParams({
+    offset: 0,
+    pageSize: 3,
+  });
+  const result = await request.get(`${baseUrl}?${query}`);
+
+  return result;
+};
