59ad2effd63f72c88e570d8fec5f9eb47ac9ca30
EnesHHayrula <eneshayrula@gmail.com>
2023-12-06 20:35:04 +0200

    mapped products so can be loaded dynamically

--- biohend/src/components/products/Products.jsx
+++ biohend/src/components/products/Products.jsx
@@ -1,20 +1,45 @@
-export const Products = [
-  {
-    id: 1,
-    image: <img src="/images/product1" alt="" />,
-    price: 10,
-    title: "Lavender oil",
-  },
-  {
-    id: 2,
-    image: <img src="/images/product2" alt="" />,
-    price: 5,
-    title: "Lavender water",
-  },
-  {
-    id: 3,
-    image: <img src="/images/product3" alt="" />,
-    price: 10,
-    title: "Worm Tea",
-  },
-];
+import React from "react";
+
+import { FaRegHeart } from "react-icons/fa";
+
+import pr1 from "/images/product1.jpg";
+import pr2 from "/images/product2.jpg";
+import pr3 from "/images/product3.jpg";
+import Card from "../card/Card";
+
+export const Products = () => {
+  const data = [
+    {
+      id: 1,
+      img: pr1,
+      price: 10,
+      title: "Lavender oil",
+    },
+    {
+      id: 2,
+      img: pr2,
+      price: 5,
+      title: "Lavender water",
+    },
+    {
+      id: 3,
+      img: pr3,
+      price: 10,
+      title: "Worm Tea",
+    },
+  ];
+
+  return (
+    <>
+      <div className="shop-box-inner">
+        <div className="container">
+          <div className="row">
+            {data?.map((item) => (
+              <Card item={item} key={item.id} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
--- biohend/src/components/shop/Shop.jsx
+++ biohend/src/components/shop/Shop.jsx
@@ -1,19 +1,17 @@
 import "./Shop.css";
-import { FaRegHeart } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import React from "react";
-import { PRODUCTS } from "../products/Products";
+import { Products } from "../products/Products";
 
 const Shop = () => {
   // Vzimane na dannite ot favorites za stranicata, za kolichkata e syshtata operaciq
-  let favorites = localStorage.getItem("favorites") ?? [];
-  let result = [];
-  PRODUCTS.forEach((product, index) => {
-    if (favorites.indexOf(product.id) !== -1) {
-      result.push(product);
-    }
-  });
-  console.log(result);
+  // let favorites = localStorage.getItem("favorites") ?? [];
+  // let result = [];
+  // Products.forEach((product, index) => {
+  //   if (favorites.indexOf(product.id) !== -1) {
+  //     result.push(product);
+  //   }
+  // });
   // Pokazvane na dannite primer kak da gi vzemesh
   // {result.map((item, index) => {
   //   return (
@@ -26,186 +24,70 @@ const Shop = () => {
   //   );
   // })}
 
-  const addToCartHandler = (e) => {
-    let items = JSON.parse(localStorage.getItem("shoppingCart")) ?? [];
+  
 
-    items.push({
-      id: $(e.target).parents(".product").attr("data-id"),
-      qty: 1,
-    });
-    localStorage.setItem("shoppingCart", JSON.stringify(items));
-  };
+  // const removeFromCartHandler = (e) => {
+  //   let items = JSON.parse(localStorage.getItem("shoppingCart")) ?? [];
 
-  const removeFromCartHandler = (e) => {
-    let items = JSON.parse(localStorage.getItem("shoppingCart")) ?? [];
+  //   if (items.indexOf($(e.target).parents(".product").attr("data-id")) > -1) {
+  //     items.splice(index, 1);
+  //     localStorage.setItem("shoppingCart", JSON.stringify(items));
+  //   }
+  // };
 
-    if (items.indexOf($(e.target).parents(".product").attr("data-id")) > -1) {
-      items.splice(index, 1);
-      localStorage.setItem("shoppingCart", JSON.stringify(items));
-    }
-  };
+  // const increaseQuantityHandler = (e) => {
+  //   let items = JSON.parse(localStorage.getItem("shoppingCart"));
+  //   let id = $(e.target).parents(".product").attr("data-id");
 
-  const increaseQuantityHandler = (e) => {
-    let items = JSON.parse(localStorage.getItem("shoppingCart"));
-    let id = $(e.target).parents(".product").attr("data-id");
+  //   items.forEach((item, index) => {
+  //     if (item.id == id) {
+  //       item.qty++;
+  //     }
+  //   });
+  //   localStorage.setItem("shoppingCart", JSON.stringify(items));
+  // };
 
-    items.forEach((item, index) => {
-      if (item.id == id) {
-        item.qty++;
-      }
-    });
-    localStorage.setItem("shoppingCart", JSON.stringify(items));
-  };
+  // const decreaseQuantityHandler = (e) => {
+  //   let items = JSON.parse(localStorage.getItem("shoppingCart"));
+  //   let id = $(e.target).parents(".product").attr("data-id");
+  //   let removeIndex = 0;
+  //   items.forEach((item, index) => {
+  //     if (item.id == id) {
+  //       item.qty--;
+  //       if (item.qty == 0) {
+  //         removeIndex = index;
+  //       }
+  //     }
+  //   });
 
-  const decreaseQuantityHandler = (e) => {
-    let items = JSON.parse(localStorage.getItem("shoppingCart"));
-    let id = $(e.target).parents(".product").attr("data-id");
-    let removeIndex = 0;
-    items.forEach((item, index) => {
-      if (item.id == id) {
-        item.qty--;
-        if (item.qty == 0) {
-          removeIndex = index;
-        }
-      }
-    });
+  //   if (removeIndex) {
+  //     delete items[removeIndex];
+  //   }
+  //   localStorage.setItem("shoppingCart", JSON.stringify(items));
+  // };
 
-    if (removeIndex) {
-      delete items[removeIndex];
-    }
-    localStorage.setItem("shoppingCart", JSON.stringify(items));
-  };
+  // const favoriteEventHandler = (e) => {
+  //   let id = $(e.target).parents(".product").attr("data-id");
+  //   let items = JSON.parse(localStorage.getItem("favorites")) ?? [];
+  //   let index = items.indexOf(id);
 
-  const favoriteEventHandler = (e) => {
-    let id = $(e.target).parents(".product").attr("data-id");
-    let items = JSON.parse(localStorage.getItem("favorites")) ?? [];
-    let index = items.indexOf(id);
+  //   if (!items.length || index == -1) {
+  //     items.push(id);
+  //     localStorage.setItem("favorites", JSON.stringify(items));
+  //     return;
+  //   }
 
-    if (!items.length || index == -1) {
-      items.push(id);
-      localStorage.setItem("favorites", JSON.stringify(items));
-      return;
-    }
-
-    if (index > -1) {
-      items.splice(index, 1);
-      localStorage.setItem("favorites", JSON.stringify(items));
-    }
-  };
-
+  //   if (index > -1) {
+  //     items.splice(index, 1);
+  //     localStorage.setItem("favorites", JSON.stringify(items));
+  //   }
+  // };
+const param = useParams()
+console.log(param);
   return (
     <div>
       <>
-        {/* Start Shop Page  */}
-        <div className="shop-box-inner">
-          <div className="container">
-            <div className="row">
-              <div className="col-sm-6 col-md-4 col-lg-4 col-xs-12">
-                <div className="product" data-id="1">
-                  <span
-                    className="product-image"
-                    style={{ backgroundImage: "url(/images/product3.jpg)" }}
-                  >
-                    {" "}
-                  </span>
-                  <div className="col-xs-12 text-center">
-                    <h3>Worm Tea</h3>
-                  </div>
-                  <div className="row">
-                    <div className="col-xs-12 col-md-6 product-icons">
-                      <span>$10.00</span>
-                    </div>
-                    <div className="col-xs-12 col-md-6 text-right product-icons">
-                      <a
-                        href="#"
-                        onClick={favoriteEventHandler}
-                        data-toggle="tooltip"
-                        data-placement="right"
-                        title="Add to Wishlist"
-                      >
-                        <FaRegHeart />
-                      </a>
-                    </div>
-                  </div>
-                  <div className="col-xs-12 text-center">
-                    <a className="cart" href="#" onClick={addToCartHandler}>
-                      Add to Cart
-                    </a>
-                  </div>
-                </div>
-              </div>
-              <div className="col-sm-6 col-md-4 col-lg-4 col-xs-12">
-                <div className="product" data-id="2">
-                  <span
-                    className="product-image"
-                    style={{ backgroundImage: "url(/images/product2.jpg)" }}
-                  >
-                    {" "}
-                  </span>
-                  <div className="col-xs-12 text-center">
-                    <h3>Lavender Oil</h3>
-                  </div>
-                  <div className="row">
-                    <div className="col-xs-12 col-md-6 product-icons">
-                      <span>$10.00</span>
-                    </div>
-                    <div className="col-xs-12 col-md-6 text-right product-icons">
-                      <a
-                        href="#"
-                        onClick={favoriteEventHandler}
-                        data-toggle="tooltip"
-                        data-placement="right"
-                        title="Add to Wishlist"
-                      >
-                        <FaRegHeart />
-                      </a>
-                    </div>
-                  </div>
-                  <div className="col-xs-12 text-center">
-                    <a className="cart" href="#" onClick={addToCartHandler}>
-                      Add to Cart
-                    </a>
-                  </div>
-                </div>
-              </div>
-              <div className="col-sm-6 col-md-4 col-lg-4 col-xs-12">
-                <div className="product" data-id="3">
-                  <span
-                    className="product-image"
-                    style={{ backgroundImage: "url(/images/product1.jpg)" }}
-                  >
-                    {" "}
-                  </span>
-                  <div className="col-xs-12 text-center">
-                    <h3>Lavender Water</h3>
-                  </div>
-                  <div className="row">
-                    <div className="col-xs-12 col-md-6 product-icons">
-                      <span>$10.00</span>
-                    </div>
-                    <div className="col-xs-12 col-md-6 text-right product-icons">
-                      <a
-                        href="#"
-                        onClick={favoriteEventHandler}
-                        data-toggle="tooltip"
-                        data-placement="right"
-                        title="Add to Wishlist"
-                      >
-                        <FaRegHeart />
-                      </a>
-                    </div>
-                  </div>
-                  <div className="col-xs-12 text-center">
-                    <a className="cart" href="#" onClick={addToCartHandler}>
-                      Add to Cart
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        {/* End Shop Page */}
+        <Products />
 
         <Link
           href="#"
