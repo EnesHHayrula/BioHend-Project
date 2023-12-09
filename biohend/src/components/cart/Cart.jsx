390784a7cfb7f7caa30bebdf8ac677815f8a4648
EnesHHayrula <eneshayrula@gmail.com>
2023-12-09 05:29:56 +0200

    cart reset and remove item from cart implemented

--- biohend/src/components/cart/Cart.jsx
+++ biohend/src/components/cart/Cart.jsx
@@ -1,25 +1,26 @@
 import React, { useContext, useState } from "react";
-import { CiSquareRemove } from "react-icons/ci";
-import { Link, useNavigate, useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { RiDeleteBin5Line } from "react-icons/ri";
 import "./Cart.css";
+import { removeItem, resetCart } from "../../redux/cartReducer";
+import { useDispatch } from "react-redux";
 
 const Cart = () => {
   const products = useSelector((state) => state.cart.products);
+  const dispatch = useDispatch();
 
   const totalPrice = () => {
     let total = 0;
     products.forEach((item) => (total += item.quantity * item.price));
     return total.toFixed(2);
   };
+
   return (
     <>
       <div className="shopping-cart">
         <h1>Products in your cart</h1>
         {products?.map((item) => (
           <div className="item" key={item.id}>
-            <img src={item.img} alt="" />
             <div className="details">
               <h1>{item.title}</h1>
               <p>{item.description?.substring(0, 100)}</p>
@@ -27,7 +28,10 @@ const Cart = () => {
                 {item.quantity} x ${item.price}
               </div>
             </div>
-            <RiDeleteBin5Line className="delete" />
+            <RiDeleteBin5Line
+              className="delete"
+              onClick={() => dispatch(removeItem(item.id))}
+            />
           </div>
         ))}
         <div className="total">
@@ -35,7 +39,10 @@ const Cart = () => {
           <span>$ {totalPrice()}</span>
         </div>
         <button>CHECKOUT</button>
-        <span className="reset"> Reset Cart</span>
+        <span className="reset" onClick={() => dispatch(resetCart())}>
+          {" "}
+          Reset Cart
+        </span>
       </div>
     </>
   );
